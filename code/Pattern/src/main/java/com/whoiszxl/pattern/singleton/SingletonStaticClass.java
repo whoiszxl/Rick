36fe7fe267e00af077370cd6e836f114abab197a
zhouxiaolong <whoiszxl@gmail.com>
2018-03-16 23:40:33 +0800

    good night,wjj

--- code/Pattern/src/main/java/com/whoiszxl/pattern/singleton/SingletonStaticClass.java
+++ code/Pattern/src/main/java/com/whoiszxl/pattern/singleton/SingletonStaticClass.java
@@ -1,5 +1,7 @@
 package com.whoiszxl.pattern.singleton;
 
+import java.io.ObjectStreamException;
+
 /**
  * 单例模式之静态内部类模式
  * @author Administrator
@@ -8,13 +10,22 @@ package com.whoiszxl.pattern.singleton;
 public class SingletonStaticClass {
 
 	
-	private SingletonStaticClass() {}
+	private SingletonStaticClass() {
+		//防止反射创建
+		if(ClassInstance.INSTANCE != null){
+			throw new RuntimeException();
+		}
+	}
 	
 	private static class ClassInstance {
 		private static final SingletonStaticClass INSTANCE = new SingletonStaticClass();
 	}
 	
 	public static SingletonStaticClass getInstance(){
+		return ClassInstance.INSTANCE;
+	}
+	
+	private Object readResolve() throws ObjectStreamException{
 		return ClassInstance.INSTANCE;
 	}
 }
