738cca869aa673338e511d18c948be98842c1bb1
zhouxiaolong <whoiszxl@gmail.com>
2018-03-19 10:36:39 +0800

    正常读取prop

--- code/XLORM/src/main/java/com/whoiszxl/xlorm/bean/Configuration.java
+++ code/XLORM/src/main/java/com/whoiszxl/xlorm/bean/Configuration.java
@@ -6,5 +6,111 @@ package com.whoiszxl.xlorm.bean;
  *
  */
 public class Configuration {
+	
+	/**
+	 * 驱动名称
+	 */
+	private String driver;
+	
+	/**
+	 * 数据库连接url
+	 */
+	private String url;
+	
+	/**
+	 * 数据库用户名
+	 */
+	private String username;
+	
+	/**
+	 * 数据库密码
+	 */
+	private String password;
+	
+	/**
+	 * 数据库名称
+	 */
+	private String useDB;
+	
+	/**
+	 * 项目源代码路径
+	 */
+	private String srcPath;
+	
+	/**
+	 * po类包名
+	 */
+	private String poPackage;
+	
+	public Configuration() {
+	}
 
+	public Configuration(String driver, String url, String username, String password, String useDB, String srcPath,
+			String poPackage) {
+		super();
+		this.driver = driver;
+		this.url = url;
+		this.username = username;
+		this.password = password;
+		this.useDB = useDB;
+		this.srcPath = srcPath;
+		this.poPackage = poPackage;
+	}
+
+	public String getDriver() {
+		return driver;
+	}
+
+	public void setDriver(String driver) {
+		this.driver = driver;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUseDB() {
+		return useDB;
+	}
+
+	public void setUseDB(String useDB) {
+		this.useDB = useDB;
+	}
+
+	public String getSrcPath() {
+		return srcPath;
+	}
+
+	public void setSrcPath(String srcPath) {
+		this.srcPath = srcPath;
+	}
+
+	public String getPoPackage() {
+		return poPackage;
+	}
+
+	public void setPoPackage(String poPackage) {
+		this.poPackage = poPackage;
+	}	
+	
 }
--- code/XLORM/src/main/java/com/whoiszxl/xlorm/core/DBManager.java
+++ code/XLORM/src/main/java/com/whoiszxl/xlorm/core/DBManager.java
@@ -1,10 +1,38 @@
 package com.whoiszxl.xlorm.core;
 
+import java.io.IOException;
+import java.util.Properties;
+
+import com.whoiszxl.xlorm.bean.Configuration;
+
 /**
  * 根据配置信息,维持连接对象的管理(具有连接池功能)
  * @author whoiszxl
  *
  */
 public class DBManager {
+	
+	/**
+	 * 数据库配置信息
+	 */
+	private static Configuration conf;
+	
+	static {//仅加载一次
+		Properties pros = new Properties();
+		try {
+			pros.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("db.properties"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		conf = new Configuration();
+		conf.setDriver(pros.getProperty("datasource.driver-class-name"));
+		conf.setPoPackage(pros.getProperty("orm.poPackage"));
+		conf.setUsername(pros.getProperty("datasource.username"));
+		conf.setPassword(pros.getProperty("datasource.password"));
+		conf.setUrl(pros.getProperty("datasource.url"));
+		conf.setSrcPath(pros.getProperty("orm.srcPath"));
+		conf.setUseDB(pros.getProperty("datasource.dbtype"));
+	}
 
+	
 }
