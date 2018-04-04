be7c515490b279e03d708a915ac27ed46843c56d
zhouxiaolong <whoiszxl@gmail.com>
2018-04-04 16:46:00 +0800

    springboot自带日志和log4j

--- 中小型后台服务架构和运维架构/project_code/whoiszxl-springboot/src/main/java/com/whoiszxl/controller/UserController.java
+++ 中小型后台服务架构和运维架构/project_code/whoiszxl-springboot/src/main/java/com/whoiszxl/controller/UserController.java
@@ -8,6 +8,7 @@ import java.util.Map;
 
 import javax.annotation.Generated;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,11 +28,13 @@ import io.swagger.annotations.ApiOperation;
 public class UserController {
 	
 	static Map<Integer, Users> a = Collections.synchronizedMap(new HashMap<Integer, Users>());
-	
+	private static Logger logger = Logger.getLogger(UserController.class); 
 	
 	@ApiOperation(value = "获取用户列表") //swagger2的注释
 	@GetMapping("/list")
 	public List<Users> getUserList() {
+		logger.info("info logger");
+		logger.error("info error");
 		a.put(1, new Users("chenhuixian", 23));
 		a.put(2, new Users("long", 22));
 		List<Users> users = new ArrayList<Users>(a.values());
