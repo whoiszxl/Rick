a28177cdbba0a9571bab97d28225a92d6e963497
zhouxiaolong <whoiszxl@gmail.com>
2018-05-06 00:05:40 +0800

    java线程并发：添加标记注解

---
+++ Java高并发学习/code/concurrency/src/main/java/com/whoiszxl/annotations/NotRecommend.java
@@ -0,0 +1,18 @@
+package com.whoiszxl.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 标记为不推荐类or写法的注解
+ * @author whoiszxl
+ *
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)//在编译时候忽略
+public @interface NotRecommend {
+
+	String value() default "";
+}
