04607a65aca572dc5230b17c90d9d18aa96f6175
zhouxiaolong <whoiszxl@gmail.com>
2018-07-31 14:13:30 +0800

    jms

---
+++ 分布式事务解决一致性问题/code/spring-tx-jms/src/main/java/com/whoiszxl/SpringTxJmsApplication.java
@@ -0,0 +1,12 @@
+package com.whoiszxl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTxJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTxJmsApplication.class, args);
+	}
+}
